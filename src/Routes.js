e86a5b24c0652e80574166f9b111eb0559c271f8
casualshay <akshaysprabhakar@gmail.com>
2020-08-30 16:11:45 -0700

    progress

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 
 class Routes extends React.Component {
   render() {
     return ( 
       <Switch>
         <Route exact path='/' component = { Home } />
+        <Route exact path='/about' component = { About } />
       </Switch>
     );
   };
