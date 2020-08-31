f347d3fe37589ad9995464c1e7f54d7450f0d0f8
casualshay <akshaysprabhakar@gmail.com>
2020-08-31 11:10:49 -0700

    home page in progess

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome!</h1>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Food Basket</h1>
       <h3>Providing food for those in need</h3>
+      <form>
+        <input placeholder="email" />
+        <br />
+        <input placeholder="password" />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -5,13 +5,13 @@ const Nav = () => {
   return (
     <header>
       <nav>
-        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/'>Logo</NavLink>
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/donate'>Donate</NavLink>
         <NavLink to='/contactus'>Contact Us</NavLink>
       </nav>
     </header>
   );
-}
+};
 
 export default Nav;
