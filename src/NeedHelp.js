6d5aaab0c051a112d8ed4d1ba52a2d1141e479b0
coolrunner123 <Nandita1227@gmail.com>
2020-08-31 13:48:30 -0400

    added MapPage and NeedHrlp

--- src/NeedHelp.js
+++ src/NeedHelp.js
@@ -1,73 +1,88 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import MapPage from '../src/MapPage';
+import { App } from './App';
 
 
-function NeedHelp() {
-  return (
-    <div className="NeedHelp">
-    <h1> Need Help? </h1>
-      <form>
-     <label>
-      Name:
-    <input type="text" name="name" />
-    </label>
-    <br></br>
-    <br></br>
-    </form>
-    <form>
-     <label>
-      Address:
-    <input type="text" name="name" />
-    </label>
-    <br></br>
-    <br></br>
-    </form>
-    <form>
-     <label>
-      Contact Person:
-    <input type="text" name="name" />
-    </label>
-    <br></br>
-    <br></br>
-    </form>
-    <form>
-     <label>
-     Phone Number:
-    <input type="text" name="name" />
-    </label>
-    <br></br>
-    <br></br>
-    </form>
+class NeedHelp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      lat: -1.2884,
+      long: 36.8233,
+      sortBy: 'best_match'
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLatChange = this.handleLatChange.bind(this);
+    this.handleLongChange = this.handleLongChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+ 
+
     
-    <h1>Details About Situaton </h1>
+
+  
+  }
+  
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  handleLatChange(event) {
+    this.setState({location: event.target.value});
+  }
+  handleLongChange(event) {
+    this.setState({location: event.target.value});
+  }
+
+  handleSearch(event) {
+    this.setState(this.state.term, this.state.lat,this.state.long);
+
+    event.preventDefault();
+  }
+
+  
+
+  render() {
+    return (
+    
+      <div className="SearchBar">
+         
+        <div className="SearchBar-sort-options">
+         
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Name" onChange={this.handleTermChange} />
+          <input placeholder="Lat?" onChange={this.handleLatChange}/>
+          <input placeholder="Long?" onChange={this.handleLongChange}/>
+        </div>
+        <label>
+        <h3>Details About Situaton </h3>
     
     <label>
-      How Many People Need Meals?
+      How Many People Can We Serve?
      <input type="text" name="name" />
     </label>
     
     <br></br>
     <br></br>
     
-    <label>
-      How Urgent?
-     <input type="text" name="name" />
-     
-    </label>
+    
     <br></br>
     <br></br>
-    <label>
     <strong>Add Me to Food Basket Map! </strong>
     <input type="submit" value="Submit" />
     </label>
-    </div>
-    
-     
-  );
-  
-
-
+        <div className="SearchBar-submit">
+        <MapPage place={this.state.term} latVal = {this.state.lat} longVal = {this.state.long}/>
+          <a onClick={this.handleSearch}>Let's Go</a>
+        </div>
+      
+      </div>
+    );
+  }
 }
-
 
 export default NeedHelp;
