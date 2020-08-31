bd8ac6835c74baeb71df2d809648725a84d3379c
Nandita K <41275398+CoolRunner123@users.noreply.github.com>
2020-08-30 22:08:38 -0400

    Add files via upload

---
+++ src/Levels.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+function Levels() {
+  return (
+    <div className="Levels">
+    <h1> Levels </h1>
+    <br></br>
+   
+    <h3> Awesome Restaurant</h3>
+    <h3>Level 10</h3>
+    <text> Helped <strong>30</strong> locations in need!</text>
+
+    <br></br>
+    <br></br>
+    <h3> Cool Restaurant</h3>
+    <h3>Level 5</h3>
+    <text> Helped <strong>15</strong> locations in need!</text>
+
+    <h1>Add cool graphics and styling to make this page less depressing</h1>
+    </div>
+    
+    
+     
+  );
+  
+
+
+}
+
+
+export default Levels;
