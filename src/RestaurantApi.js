52e0affd3ae369ad740460cc93c00752f5bc6956
Mimi <mimitraugott@gmail.com>
2020-09-01 23:44:11 -0600

    notes in restaurant api

--- src/RestaurantApi.js
+++ src/RestaurantApi.js
@@ -1,6 +1,17 @@
 import axios from "axios";
 
+
+/**
+ * not sure if this is going to be used yet. haven't decided if forms will post directly to server or if we will use this api
+ * @param {*} params 
+ */
 export const addRestaurant = (params) => {};
+
+
+/**
+ * this function will get the list of all restaurants in the database. 
+ * @param {*} cb - standard callback(err, results)
+ */
 
 export const getRestaurants = (cb) => {
     console.log("starting call to getrest")
