30fe12e596af35c69211dd4d0dc362b0c48e74e4
casualshay <akshaysprabhakar@gmail.com>
2020-09-02 15:14:18 -0700

    complete?

--- src/MapPage.js
+++ src/MapPage.js
@@ -1,50 +1,46 @@
 import React, { Component } from 'react';
 import { Map,GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import NeedHelp from '../src/NeedHelp';
-import longVal from '../src/NeedHelp';
-import latVal from '../src/NeedHelp';
-
 const mapStyles = {
   width: '50%',
   height: '50%'
 };
-
-class MapPage extends React.Component {  
+class MapPage extends React.Component {
   constructor() {
-    super();  
+    super();
     this.state = {
-      lat: latVal,
-      lng: longVal,
-      showingInfoWindow: false,  //Hides or the shows the infoWindow
-      activeMarker: {},          //Shows the active marker upon click
-      selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
-    };
-  }
-  onMarkerClick (props, marker, e) {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+}
+  onMarkerClick(props, marker, e) {
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+}
+onClose(props) {
+  if (this.state.showingInfoWindow) {
     this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
+      showingInfoWindow: false,
+      activeMarker: null
     });
   }
-  onClose(props) {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
-    }
-  };
+};
   render() {
+    console.log(this.props);
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         latVal,longVal //it works when you put in actual coordinates
-        }}
-      >
+      !this.props.longVal && !this.props.latVal ? null : <Map
+      google={this.props.google}
+      zoom={14}
+      style={mapStyles}
+      initialCenter={{
+      lat: 35,
+      lng: 100 //it works when you put in actual coordinates
+      }}
+    >
       <Marker
           onClick={this.onMarkerClick}
           name={this.props.place}
@@ -58,11 +54,10 @@ class MapPage extends React.Component {
             <h4>{this.state.selectedPlace.name}</h4>
         </div>
       </InfoWindow>
-      </Map> 
+      </Map>
     );
   }
 }
-
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDl6NClnt601rlaqaGeTdPU_XSZtYNWd2o'
+  apiKey: 'AIzaSyA9QVRCFi7gUdzwot0oueSSy90hybtqNpM'
 })(MapPage);
--- src/NeedHelp.js
+++ src/NeedHelp.js
@@ -1,37 +1,39 @@
 import React from 'react';
 import MapPage from '../src/MapPage';
+import { App } from './App';
 import Geocode from "react-geocode";
-
-Geocode.setApiKey("AIzaSyDl6NClnt601rlaqaGeTdPU_XSZtYNWd2o");
-
+// import Logo from "../logo.PNG";
+// import background from "./background.jpg";
+import Button from 'react-bootstrap/Button';
+Geocode.setApiKey("AIzaSyA9QVRCFi7gUdzwot0oueSSy90hybtqNpM");
+Geocode.setLanguage("en");
+// set response region. Its optional.
+// A Geocoding request with region=es (Spain) will return the Spanish city.
+// Enable or disable logs. Its optional.
 class NeedHelp extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       term: '',
-      latitude: -1.2884, //default
-      longitude: 36.8233,
+      latitude: null, //default
+      longitude: null,
       list: [0,1],
-      number: 2, 
-      sortBy: 'best_match'
+      number: 0, 
+      location:""
     }
-
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
-    this.handleNumberChange = this.handleNumberChange.bind(this); 
+    this.handleNumberChange = this.handleNumberChange.bind(this);
   }
   //this is used to change from address to cordinates
-  Converter(location){
-  Geocode.fromAddress(location).then(
+  Converter(location1){
+    console.log(this.state.location)
+  location1 = this.state.location;
+  Geocode.fromAddress(location1).then(
     response => {
       const { lat, lng } = response.results[0].geometry.location;
-      console.log(lat, lng, 'yo');
-      // this.state = {
-      //   latitude: lat,
-      //   longitude:lng,
-      // }
+      console.log(lat, lng);
       this.setState({
        latitude: lat,
        longitude: lng
@@ -42,67 +44,81 @@ class NeedHelp extends React.Component {
     }
   );
   }
-
   handleTermChange(event) {
     this.setState({term: event.target.value});
   }
   handleNumberChange(event) {
     this.setState({number: event.target.value});
   }
-
   handleLocationChange(event) {
-   this.Converter(event);
+  this.setState({location: event.target.value});
+   this.Converter();
   }
   handleSearch(event) {
-    this.setState(this.state.term, this.state.latitude,this.state.longitude, this.state.number);
-
+    this.setState(this.state.term, this.state.lat,this.state.lng, this.state.number,this.state.location);
     event.preventDefault();
   }
-
  GoodJob(value) {
   value = this.state.number;
     alert("Thank you for feeding "+ value +" people today!"+
-    " Make sure to follow social distancing protocols when getting your food")
-   
+    "    Make sure to follow social distancing protocols                                       WEAR A MASK      MAINTAIN SOCIAL DISTANCING           ")
   };
-
   render() {
-    console.log(this.state.term, this.state.latitude, this.state.longitude, 'hiiiiii');
-
     return (
-      <div className="SearchBar">
+      <div style = {{backgroundColor:"rgb(240, 240, 245)" ,height: 1700, top:100}} className="SearchBar">
+         {/* <img style = {{position: "absolute", height: 300, right: 250, bottom: 200}}src={background} alt= "website logo"></img> */}
+         <text style = {{position: "absolute", right: 400, bottom: 150, color:"rgb(255, 102, 102)", fontSize:25, fontStyle: "italic"}}> don't be haste in waste</text>
+         {/* <img style = {{height: 70}}src={Logo} alt= "website logo"></img> */}
+         <Button style = {{position: "absolute", left: 950, top: 20,color: "white", backgroundColor:"rgb(255, 102, 102)", height:50, width:100}}variant="outline-info">Home</Button>{' '}
+         <Button style = {{position: "absolute", left: 1100, top: 20,color: "rgb(255, 102, 102)", height:50, width:100,color: "white", backgroundColor:"rgb(255, 102, 102)"}}variant="outline-info">Contact</Button>{' '}
         <div className="SearchBar-fields">
-          <input placeholder="Name" onChange={this.handleTermChange} />
-          <input placeholder="Address?" onChange={this.handleLocationChange}/>
+        <h3>Contact Info </h3>
+          <input style = {{borderColor:"rgb(255, 102, 102)"}}placeholder="Name" onChange={this.handleTermChange} />
+          <br></br>
+          <br></br>
+          <input style = {{borderColor:"rgb(255, 102, 102)"}}placeholder="Phone Number" />
+          <br></br>
+          <br></br>
+          <input style = {{borderColor:"rgb(255, 102, 102)"}} placeholder="Address"  onChange={this.handleLocationChange}/>
         </div>
         <label>
-        <h3>Details About Situaton </h3>
+        <h3>Details About Situation </h3>
     <br></br>
     <label>
       How Many People Can We Serve?
-      <input placeholder="number" onChange={this.handleNumberChange} />
+      <br></br>
+      <input style = {{borderColor:"rgb(255, 102, 102)"}} placeholder="# of Meals" onChange={this.handleNumberChange} />
     </label>
+    <br></br>
     <br></br>
     <label>
       Delivery Information / PickUp Information
-     <input type="text" name="name" />
+      <br></br>
+     <input style = {{borderColor:"rgb(255, 102, 102)"}} placeholder="time/location"type="text" name="name" />
     </label>
     <br></br>
     <br></br>
-    <strong>Add Me to Food Basket Map! </strong>
-    <text>{this.state.latitude}</text>
-    <br></br>
-    <text>{this.state.longitude}</text>
-    <button  type="submit" value="Submit" onClick={this.GoodJob}>
-      Submit
-    </button>
+    <strong>Add Me to Food Basket Map! <span>&nbsp;&nbsp;</span></strong>
+    <button type="submit" value="Submit" onClick={this.GoodJob} style = {{position: "absolute", color: "rgb(255, 102, 102)", height:40, width:100,color: "white", backgroundColor:"rgb(255, 102, 102)"}}>
+  Submit
+</button>
+<br></br>
+<br></br>
     </label>
-      <div className="SearchBar-submit">
-        <MapPage place={this.state.term} latVal={this.state.latitude} longVal={this.state.longitude}/>
-      </div>
+    <br></br>
+        <div className="SearchBar-submit">
+        </div>
+        <div>
+{/*     
+    <h1 style={{color: "red" ,textAlign: "right"}}>LeaderBoard</h1>
+    <h3 style={{textAlign: "right"}}> Number 1: {this.state.term}</h3>
+    <h5 style={{textAlign: "right"}}> Served: {this.state.number} meals! </h5> */}
+<div >
+<MapPage place={this.state.term} latVal={this.state.latitude} longVal={this.state.longitude}/>
+</div>
+        </div>
       </div>
     );
   }
 }
-
 export default NeedHelp;
