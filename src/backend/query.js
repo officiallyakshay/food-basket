6481ca0563313d9cd2948ee8700c63bcacdddf7d
chinmayeebandal <cbandal@buffalo.edu>
2020-09-01 23:24:07 -0400

    some stuff

--- src/backend/query.js
+++ src/backend/query.js
@@ -13,28 +13,37 @@ let db = new sqlite3.Database('../../database/tinyDB.db', sqlite3.OPEN_READWRITE
   });
 
 
-db.serialize(() => {
-    db.each(`SELECT * FROM restaurants`, (err, row) => {
-      if (err) {
-        console.error(err.message);
-      }
-      var rest = new Restaurant(
-          name = row.name,
-          quantity = row.quantity,
-          address = row.address,
-          contact = row.contact,
-          start = row.start,
-          end = row.end
-    );
+var readRecords = function(callback) {
+    let db = new sqlite3.Database('../../database/tinyDB.db', sqlite3.OPEN_READWRITE);
 
-      console.log(rest.name + "\t");
-      //console.log(rest + "\t");       //Prints out the Restaurant Object called rest. 
+    db.serialize(() => {
+        db.each(`SELECT * FROM restaurants`, (err, row) => {
+          if (err) {
+            console.error(err.message);
+            callback(err);
+          }
+          var rest = new Restaurant(
+              name = row.name,
+              quantity = row.quantity,
+              address = row.address,
+              contact = row.contact,
+              start = row.start,
+              end = row.end
+         );
+         console.log(util.inspect(row));
+         console.log(rest.name + "\t")
+         //console.log(rest + "\t");       //Prints out the Restaurant Object called rest. 
+          callback(row);
+       });
     });
-  });
 
-  db.close((err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
+    db.close((err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+}
+
+
+
