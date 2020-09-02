c6d5d42f52e938bacdcd87c1fa94eb91866575cc
chinmayeebandal <cbandal@buffalo.edu>
2020-09-02 13:39:37 -0400

    Added recipient controller

--- server/database/query.js
+++ server/database/query.js
@@ -3,18 +3,6 @@ var util = require('util');
 let Restaurant = require('./restaurant.js');
 const path = require('path')
 
-//use for debugging purposes
-/* 
-let db = new sqlite3.Database('../../database/tinyDB.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    else{
-        console.log('Connected to the tinyDB database.');
-    }
-    
-  });
-*/
 
 //Populates the data into the Restaurant object and returns in a callback function. Will be useful
 //for displaying data on the website 
@@ -26,7 +14,7 @@ var readRestaurants = function(callback) {
     db.close()
 }
 
-var readRestaurants = function(callback) {
+var readRecipients = function(callback) {
   let db = new sqlite3.Database('./server/database/reci.db', sqlite3.OPEN_READWRITE);
 
   let results = []
@@ -35,7 +23,8 @@ var readRestaurants = function(callback) {
 }
 
 module.exports = {
-  readRestaurants
+  readRestaurants,
+  readRecipients
 }
 
 //Use for debugging purposes
