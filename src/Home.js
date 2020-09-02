3c01edaee059e9fa23d652909f18b35fe51edfe5
casualshay <akshaysprabhakar@gmail.com>
2020-09-02 10:42:12 -0700

    landing page, restaurant, ui, almost done

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,11 @@ class Home extends React.Component {
     const { onClick } = this;
     return (
       <div style={{ textAlign: 'center' }}>
-        <h1>Food Basket</h1>
-        <h3>Saving Food To Save Lives</h3>
-        <div>
+        <div className="home-text">
+          <h1>Food Basket</h1>
+          <h3>Saving Food To Save Lives</h3>
+        </div>
+        <div className="form">
           <form>
             <button onClick={(ev) => onClick(ev)}>
               <Link to='/restaurant'>Feed Now</Link>
