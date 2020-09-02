8de30b7ade3e8de1d24a4e8caa6a72dffbd9e6b6
Nandita K <41275398+CoolRunner123@users.noreply.github.com>
2020-09-01 22:53:54 -0400

    Add files via upload
    Donation Page for Restaurants (Need Help) is fully functional and styled. Map worls

--- ThankYou.js
+++ ThankYou.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from "./logo.PNG";
+import Logo from "./logo.PNG";
 import mask from "./maskpic.PNG";
 import Button from 'react-bootstrap/Button';
 
@@ -10,7 +10,7 @@ class ThankYou extends React.Component {
     return (
      <div style = {{backgroundColor:"white"}}>
         <div style = {{backgroundColor:"rgb(240, 240, 245)" ,height: 1000, top:100}}>
-         <img style = {{height: 70}}src={logo}></img>
+         <img style = {{height: 70}}src={Logo} alt= "website logo"></img>
          <Button style = {{position: "absolute", left: 950, top: 20,color: "white", backgroundColor:"rgb(255, 102, 102)", height:50, width:100}}variant="outline-info">Home</Button>{' '}
          <Button style = {{position: "absolute", left: 1100, top: 20,color: "rgb(255, 102, 102)", height:50, width:100,color: "white", backgroundColor:"rgb(255, 102, 102)"}}variant="outline-info">Contact</Button>{' '}
          <h1 style = {{position:"absolute", left: 455, fontSize: 80, color:"rgb(255, 102, 102)"}}>THANK YOU</h1>
@@ -18,6 +18,7 @@ class ThankYou extends React.Component {
          <img style = {{position:"absolute",height: 300, top:300,left:200}}src={mask}></img>
          <text style = {{position:"absolute", left: 520, top: 450,fontSize: 20,color:"rgb(255, 102, 102)"}}>Follow safety precautions to <strong>prevent the spread</strong> </text>
          </div>
+         
      </div>
       
     );
